f20caca73387025b2ede7ce7056a4b3158f1c40f
Vishal Singh <vishalkumar70854@gmail.com>
2024-04-18 21:56:06 +0530

    done some changes in configServer application.yml file

--- ConfigServer/src/main/java/com/lcwd/config/server/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/lcwd/config/server/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.lcwd.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer // this annotation is used to enable configuration server
+//@EnableDiscoveryClient	// this annotation is not mandatory as by adding eureka client dependency in pom.xml this annotation is implicitly used by spring boot on startup
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
