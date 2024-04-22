c1313509644ac95e740edb9ae705756322d1702f
Vishal Singh <vishalkumar70854@gmail.com>
2024-04-22 10:43:34 +0530

    implemented spring security webflux in api-gateway using okta and spring security, successfully able to get user and client details like email, authorities, access_token, refresh_token, expiresAt

---
+++ ApiGateway/src/main/java/com/lcwd/gateway/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.lcwd.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+
+// we have used EnableWebFluxSecurity since api-gateway uses webflux and cloud starter gateway for handling request
+// that's why we have used EnableWebFluxSecurity instead of WebSecurity since api-gateway uses webflux for cloud gateway
+@Configuration      // this annotation means we can have multiple beans per method
+@EnableWebFluxSecurity  // we have used EnableWebFluxSecurity since api-gateway uses webflux and cloud starter gateway for handling request
+public class SecurityConfig {
+    @Bean   // bean of security web filter chain as we have used webflux for cloud gateway... this is different from securityFilterChain
+    SecurityWebFilterChain apiHttpSecurity(ServerHttpSecurity httpSecurity) {
+        httpSecurity
+                .authorizeExchange((exchanges) -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return httpSecurity.build();
+    }
+
+
+}
---
+++ ApiGateway/src/main/java/com/lcwd/gateway/controllers/AuthController.java
@@ -0,0 +1,63 @@
+package com.lcwd.gateway.controllers;
+
+import com.lcwd.gateway.models.AuthResponse.AuthResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+//    OAuth2AuthorizedClient will give the client details for ex: access-token value, client registration details
+//    OidcUser will give User registration details for ex: email, profile etc
+    @GetMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client,
+            @AuthenticationPrincipal OidcUser user,
+            Model model
+    ){
+
+        // we will get some objects from OKTA and from there we will return reponse of type AuthResponse
+
+        logger.info("User email id: {}", user.getEmail());
+
+        // create object of Authresponse
+        AuthResponse authResponse = new AuthResponse();
+
+        // setting email to authreponse
+        authResponse.setUserId(user.getEmail());
+
+        // setting token to authresponse
+        authResponse.setAccessToken(client.getAccessToken().getTokenValue());
+
+        //setting refreshTokenValue to authresponse
+        authResponse.setRefreshToken(client.getRefreshToken().getTokenValue());
+
+        // setting expiresAt to authresponse
+        authResponse.setExpireAt(client.getAccessToken().getExpiresAt().getEpochSecond());
+
+        // using stream we got each authority and we want List<String> so we have converted every authority to String and returned
+        List<String> authorities = user.getAuthorities().stream().map(grantedAuthority -> {
+            return grantedAuthority.getAuthority();
+        }).collect(Collectors.toList());
+
+        // setting List<authorities> to authresponse
+        authResponse.setAuthorities(authorities);
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+}
