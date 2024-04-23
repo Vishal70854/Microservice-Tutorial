2afaedc7eecde89d9e6679053f19b7cba179d618
Vishal Singh <vishalkumar70854@gmail.com>
2024-04-23 10:05:38 +0530

    added okta configuration in rating-service and hotel-service. Also added hasAuthority() to all apis of rating and hotelservice with scope internal, admin, normal

--- HotelService/src/main/java/com/lcwd/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/lcwd/hotel/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.lcwd.hotel.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +15,12 @@ import java.util.List;
 public class HotelController {
     @Autowired
     private HotelService hotelService;
+
+    // here we have defined scopes because we dont want our api's to be accessed directly by anyone. only specific users are allowed to access limited api's
+    // it is because of @EnableMethodSecurity(prePostEnabled = true)
+
     //create
+    @PreAuthorize("hasAuthority('Admin')")  // this api will not be directly called. only admin should be able to create a hotel object via this api
     @PostMapping()
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
         Hotel hotel1 = hotelService.create(hotel); // create hotel by calling hotelservice create method
@@ -23,11 +29,15 @@ public class HotelController {
 
     }
     //getAll
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")  // this api will not be directly called. it can only be called by users with scope internal or admin
     @GetMapping()
     public ResponseEntity<List<Hotel>> getAll(){
+
         return ResponseEntity.ok(hotelService.getAll());
     }
+
     //get single
+    @PreAuthorize("hasAuthority('SCOPE_internal')")  // this api will not be directly called. it can only be called by users with scope internal
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> createHotel(@PathVariable String hotelId){
         Hotel hotel1 = hotelService.get(hotelId); // create hotel by calling hotelservice create method
--- RatingService/src/main/java/com/lcwd/rating/controllers/RatingController.java
+++ RatingService/src/main/java/com/lcwd/rating/controllers/RatingController.java
@@ -5,6 +5,7 @@ import com.lcwd.rating.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class RatingController {
     private RatingService ratingService;
 
     // create rating
+    @PreAuthorize("hasAuthority('Admin')")  // this api will not be directly called. only admin should be able to create a hotel object via this api
     @PostMapping()
     public ResponseEntity<Rating> create(@RequestBody Rating rating){
         return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
@@ -27,7 +29,12 @@ public class RatingController {
         return ResponseEntity.ok(ratingService.getRatings());
     }
 
+    // here we have defined scopes because we dont want our api's to be accessed directly by anyone. only specific users are allowed to access limited api's
+    // it is because of @EnableMethodSecurity(prePostEnabled = true)
+
+
     // get all ratings by user id
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")  // this api will not be directly called. it can only be called by users with scope internal or admin
     @GetMapping("/users/{userId}")
     public ResponseEntity<List<Rating>> getRatingsByUserId(@PathVariable  String userId){
         return ResponseEntity.ok(ratingService.getRatingByUserId(userId));
